a3c15896289e967396df0f2e85525f170ded4181
Hamza El makhroubi <hel-makh@e2r11p5.1337.ma>
2022-01-17 20:09:11 +0100

    updated prototypes & structs

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 10:59:08 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/01/13 20:52:40 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/01/17 19:41:52 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,25 +26,28 @@ typedef struct s_stacks {
 	t_stack	a;
 	t_stack	b;
 	t_stack	sorted;
+	char	*instructions;
 }	t_stacks;
 
 char	*ft_strnjoin(char const *s1, char const *s2, size_t n);
 void	*ft_free(void *ptr);
 void	*ft_free_2d(char **ptr);
 void	*ft_free_3d(char ***ptr);
-void	push_swap(t_stacks *stacks);
-void    ft_sa(t_stack *a, t_stack *b, char **instructions);
-void	ft_sb(t_stack *a, t_stack *b, char **instructions);
-void	ft_ss(t_stack *a, t_stack *b, char **instructions);
-void	ft_pa(t_stack *a, t_stack *b, char **instructions);
-void	ft_pb(t_stack *a, t_stack *b, char **instructions);
-void	ft_ra(t_stack *a, t_stack *b, char **instructions);
-void	ft_rb(t_stack *a, t_stack *b, char **instructions);
-void	ft_rr(t_stack *a, t_stack *b, char **instructions);
-void	ft_rra(t_stack *a, t_stack *b, char **instructions);
-void	ft_rrb(t_stack *a, t_stack *b, char **instructions);
-void	ft_rrr(t_stack *a, t_stack *b, char **instructions);
-int     ft_get_int_index(t_stack st, int nb);
+void    ft_sa(t_stacks *stacks);
+void	ft_sb(t_stacks *stacks);
+void	ft_ss(t_stacks *stacks);
+void	ft_pa(t_stacks *stacks);
+void	ft_pb(t_stacks *stacks);
+void	ft_ra(t_stacks *stacks);
+void	ft_rb(t_stacks *stacks);
+void	ft_rr(t_stacks *stacks);
+void	ft_rra(t_stacks *stacks);
+void	ft_rrb(t_stacks *stacks);
+void	ft_rrr(t_stacks *stacks);
 t_stack	ft_sort(t_stack st);
+int     ft_get_int_index(t_stack st, int nb);
+void	push_swap(t_stacks *stacks);
+int		ft_get_biggest_stack_head(t_stacks *stacks);
+void	ft_print_instructions(char *instructions);
 
 #endif
