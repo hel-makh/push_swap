e9cac246008bf0add02b55a55591b34233532d1c
Hamza El makhroubi <hel-makh@e2r11p5.1337.ma>
2022-01-18 20:03:24 +0100

    instructions optimisation

--- srcs/ft_print_instructions.c
+++ srcs/ft_print_instructions.c
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 13:51:56 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/01/17 17:29:29 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/01/18 19:08:13 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,36 @@ static void	ft_change_instruction(char **instruction, char **next, char *new)
 	ft_bzero(*next, ft_strlen(*next));
 }
 
+static int	ft_inst_matches(char *inst1, char *inst2, char *comp1, char *comp2)
+{
+	if (!ft_strncmp(inst1, comp1, ft_strlen(comp1))
+		&& !ft_strncmp(inst2, comp2, ft_strlen(comp2)))
+		return (1);
+	else if (!ft_strncmp(inst1, comp2, ft_strlen(comp2))
+		&& !ft_strncmp(inst2, comp1, ft_strlen(comp1)))
+		return (1);
+	return (0);
+}
+
 static void	ft_optimise_instructions(char **arr)
 {
 	int	i;
 
 	i = 0;
-	while (arr[i])
+	while (arr[i + 1])
 	{
-		if (!ft_strncmp(arr[i], "sa", 2) && !ft_strncmp(arr[i + 1], "sb", 2))
+		if (ft_inst_matches(arr[i], arr[i + 1], "sa", "sb"))
 			ft_change_instruction(&arr[i], &arr[i + 1], "ss");
-		if (!ft_strncmp(arr[i], "ra", 2) && !ft_strncmp(arr[i + 1], "rb", 2))
+		if (ft_inst_matches(arr[i], arr[i + 1], "ra", "rb"))
 			ft_change_instruction(&arr[i], &arr[i + 1], "rr");
-		if (!ft_strncmp(arr[i], "rra", 3) && !ft_strncmp(arr[i + 1], "rrb", 3))
+		if (ft_inst_matches(arr[i], arr[i + 1], "rra", "rrb"))
 			ft_change_instruction(&arr[i], &arr[i + 1], "rrr");
+		if (ft_inst_matches(arr[i], arr[i + 1], "ra", "rra"))
+			ft_change_instruction(&arr[i], &arr[i + 1], "");
+		if (ft_inst_matches(arr[i], arr[i + 1], "rb", "rrb"))
+			ft_change_instruction(&arr[i], &arr[i + 1], "");
+		if (ft_inst_matches(arr[i], arr[i + 1], "pa", "pb"))
+			ft_change_instruction(&arr[i], &arr[i + 1], "");
 		i ++;
 	}
 }
@@ -46,6 +63,10 @@ void	ft_print_instructions(char *instructions)
 	ft_optimise_instructions(arr);
 	i = 0;
 	while (arr[i])
-		ft_putendl_fd(arr[i++], STDOUT_FILENO);
+	{
+		if (*arr[i])
+			ft_putendl_fd(arr[i], STDOUT_FILENO);
+		i ++;
+	}
 	arr = ft_free_2d(arr);
 }
