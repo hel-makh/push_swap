4f31bd53d11f06819e64b9a068478a3025d5e34c
Hamza El makhroubi <hel-makh@e2r11p5.1337.ma>
2022-01-18 13:04:58 +0100

    fixed norm

--- srcs/ft_get_int_index.c
+++ srcs/ft_get_int_index.c
@@ -6,13 +6,13 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 14:37:56 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/01/13 17:45:23 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/01/18 13:03:44 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int ft_get_int_index(t_stack st, int nb)
+int	ft_get_int_index(t_stack st, int nb)
 {
 	int	i;
 
