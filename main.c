c4e05deef72c55ae7be318caf16d10ceb64b48b3
Hamza El makhroubi <hel-makh@e2r11p5.1337.ma>
2022-01-21 02:02:00 +0100

    handled errors

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 11:11:10 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/01/19 21:17:11 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/01/21 01:53:37 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 // #include "srcs/ft_strnjoin.c"
 // #include "srcs/ft_free.c"
+// #include "srcs/ft_get_min_index.c"
 
 // #include "srcs/operations/sa.c"
 // #include "srcs/operations/sb.c"
@@ -39,69 +40,116 @@
 // #include "srcs/ft_print_instructions.c"
 // #include "srcs/ft_quit_program.c"
 
-static void	push_swap(t_stacks *stacks)
-{
-	ft_init_stack_b(stacks);
-	ft_sort_into_stack_a(stacks);
-	ft_print_instructions(stacks->instructions);
-}
-
-static void	ft_create_stacks(t_stacks *stacks, int size, char *argv[])
-{
-	int		i;
-
-	stacks->a.stack = ft_calloc(size, sizeof(int));
-	stacks->b.stack = ft_calloc(size, sizeof(int));
-	i = 0;
-	while (size >= 0)
-		stacks->a.stack[i++] = ft_atoi(argv[size--]);
-	stacks->a.top = i - 1;
-	stacks->b.top = 0;
-}
-
-static int	ft_is_arg_valid(int argc, char *argv[])
+static int	ft_check_duplicates(t_stack st)
 {
 	int	i;
 	int	j;
-	int	k;
 
-	i = 1;
-	while (i < argc)
+	i = 0;
+	while (i < st.top)
 	{
-		j = 0;
-		while (argv[i][j])
+		j = i + 1;
+		while (j < st.top)
 		{
-			if (!ft_isdigit(argv[i][j]) && argv[i][j] != '-')
-				return (0);
+			if (st.stack[i] == st.stack[j])
+				return (1);
 			j ++;
-		}
-		k = i + 1;
-		while (k < argc)
-		{
-			if (!ft_strncmp(argv[k], argv[i], ft_strlen(argv[k]))
-				&& !ft_strncmp(argv[k], argv[i], ft_strlen(argv[i])))
-				return (0);
-			k ++;
 		}
 		i ++;
 	}
-	return (1);
+	return (0);
+}
+
+static int	ft_args_count(char *argv[], int argc)
+{
+	char	**digits;
+	int		args_count;
+	int		i;
+	int		j;
+
+	args_count = 0;
+	i = 0;
+	while (argc >= 0)
+	{
+		digits = ft_split(argv[argc], ' ');
+		j = 0;
+		while (digits[j])
+			j ++;
+		args_count += j;
+		digits = ft_free_2d(digits);
+		argc --;
+	}
+	return (args_count);
+}
+
+static int	ft_args_iter(t_stacks *stacks, char *argv, int *index)
+{
+	char	**digits;
+	int		return_value;
+	int		i;
+
+	if (!*argv)
+		return (1);
+	digits = ft_split(argv, ' ');
+	return_value = 0;
+	i = 0;
+	while (digits[i])
+		i ++;
+	while (--i >= 0)
+	{
+		if (ft_long_atoi(digits[i]) > INT_MAX
+			|| ft_long_atoi(digits[i]) < INT_MIN
+			|| (ft_atoi(digits[i]) == 0
+				&& ft_strncmp(digits[i], "0", ft_strlen(digits[i]))
+				&& ft_strncmp(digits[i], "-0", ft_strlen(digits[i]))
+				&& ft_strncmp(digits[i], "+0", ft_strlen(digits[i]))))
+			return_value += 1;
+		stacks->a.stack[*index] = ft_atoi(digits[i]);
+		*index += 1;
+	}
+	digits = ft_free_2d(digits);
+	return (return_value);
+}
+
+static int	ft_create_stacks(t_stacks *stacks, char *argv[], int size)
+{
+	int	return_value;
+	int	args_count;
+	int	i;
+
+	return_value = 0;
+	args_count = ft_args_count(argv, size);
+	stacks->a.stack = ft_calloc(args_count, sizeof(int));
+	stacks->b.stack = ft_calloc(args_count, sizeof(int));
+	i = 0;
+	while (size > 0)
+	{
+		return_value += ft_args_iter(stacks, argv[size], &i);
+		size --;
+	}
+	stacks->a.top = i;
+	stacks->b.top = 0;
+	return (return_value);
 }
 
 int	main(int argc, char *argv[])
 {
 	t_stacks	stacks;
+	int			error;
 
-	if (argc < 2)
+	if (argc == 1)
 		return (EXIT_FAILURE);
-	if (!ft_is_arg_valid(argc, argv))
-	{
-		ft_putendl_fd("Error", STDERR_FILENO);
-		return (EXIT_FAILURE);
-	}
-	ft_create_stacks(&stacks, argc - 1, argv);
+	error = ft_create_stacks(&stacks, argv, argc - 1);
+	error += ft_check_duplicates(stacks.a);
 	stacks.sorted = ft_sort(stacks.a);
 	stacks.instructions = ft_strdup("");
-	push_swap(&stacks);
+	if (error)
+	{
+		ft_putendl_fd("Error", STDERR_FILENO);
+		ft_quit_program(EXIT_FAILURE, &stacks);
+	}
+	ft_init_stack_b(&stacks);
+	ft_sort_into_stack_a(&stacks);
+	ft_print_instructions(stacks.instructions);
 	ft_quit_program(EXIT_SUCCESS, &stacks);
 }
