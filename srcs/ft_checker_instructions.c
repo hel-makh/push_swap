c4db7dceca36f549fcec74f5b95d6a2d57246548
root <root@D4RQS1D3R.localdomain>
2022-01-27 19:05:35 +0100

    changed function name

--- srcs/ft_checker_instructions.c
+++ srcs/ft_checker_instructions.c
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 03:44:42 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/01/21 04:19:02 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/01/27 19:02:24 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ void	ft_swap(t_stacks *stacks, char *instruction)
 	if (instruction[2])
 		ft_quit_checker(EXIT_FAILURE, stacks);
 	if (!ft_strncmp(instruction, "sa", 2))
-		ft_sa(stacks);
+		sa(stacks);
 	else if (!ft_strncmp(instruction, "sb", 2))
-		ft_sb(stacks);
+		sb(stacks);
 	else if (!ft_strncmp(instruction, "ss", 2))
-		ft_ss(stacks);
+		ss(stacks);
 	else
 		ft_quit_checker(EXIT_FAILURE, stacks);
 }
@@ -31,9 +31,9 @@ void	ft_push(t_stacks *stacks, char *instruction)
 	if (instruction[2])
 		ft_quit_checker(EXIT_FAILURE, stacks);
 	if (!ft_strncmp(instruction, "pa", 2))
-		ft_pa(stacks);
+		pa(stacks);
 	else if (!ft_strncmp(instruction, "pb", 2))
-		ft_pb(stacks);
+		pb(stacks);
 	else
 		ft_quit_checker(EXIT_FAILURE, stacks);
 }
@@ -43,11 +43,11 @@ void	ft_shift_up(t_stacks *stacks, char *instruction)
 	if (instruction[2])
 		ft_quit_checker(EXIT_FAILURE, stacks);
 	if (!ft_strncmp(instruction, "ra", 2))
-		ft_ra(stacks);
+		ra(stacks);
 	else if (!ft_strncmp(instruction, "rb", 2))
-		ft_rb(stacks);
+		rb(stacks);
 	else if (!ft_strncmp(instruction, "rr", 2))
-		ft_rr(stacks);
+		rr(stacks);
 	else
 		ft_quit_checker(EXIT_FAILURE, stacks);
 }
@@ -57,11 +57,11 @@ void	ft_shift_down(t_stacks *stacks, char *instruction)
 	if (instruction[3])
 		ft_quit_checker(EXIT_FAILURE, stacks);
 	if (!ft_strncmp(instruction, "rra", 3))
-		ft_rra(stacks);
+		rra(stacks);
 	else if (!ft_strncmp(instruction, "rrb", 3))
-		ft_rrb(stacks);
+		rrb(stacks);
 	else if (!ft_strncmp(instruction, "rrr", 3))
-		ft_rrr(stacks);
+		rrr(stacks);
 	else
 		ft_quit_checker(EXIT_FAILURE, stacks);
 }
